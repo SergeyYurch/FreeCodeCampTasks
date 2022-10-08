1d83c03629fbd36b3f6b0efe4b4d77f298a256b0
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-10-08 15:08:53 +0300

    Add task61.js

--- src/task60.js
+++ src/task60.js
@@ -1,8 +1,9 @@
-function testEqual(val) {
-  if (val==12) { // Change this line
+// Setup
+function testStrict(val) {
+  if (val===7) { // Change this line
     return "Equal";
   }
   return "Not Equal";
 }
 
-testEqual(10);
+testStrict(10);
